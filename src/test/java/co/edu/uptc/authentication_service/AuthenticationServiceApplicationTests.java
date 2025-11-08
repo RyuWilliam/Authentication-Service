df92a49cfac040a6e4a9a5069d985a849bdd96b4
=William <ruyk1120@gmail.com>
2025-11-08 18:08:52 -0500

    First commit

---
+++ src/test/java/co/edu/uptc/authentication_service/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.authentication_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
