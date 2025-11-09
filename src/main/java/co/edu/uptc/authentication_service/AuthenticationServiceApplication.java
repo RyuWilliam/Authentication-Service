9a2d192d293922f4fb18b06856464b35bbab1dbf
=William <ruyk1120@gmail.com>
2025-11-09 17:24:41 -0500

    version 1

--- src/main/java/co/edu/uptc/authentication_service/AuthenticationServiceApplication.java
+++ src/main/java/co/edu/uptc/authentication_service/AuthenticationServiceApplication.java
@@ -2,6 +2,9 @@ package co.edu.uptc.authentication_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class AuthenticationServiceApplication {
@@ -9,5 +12,9 @@ public class AuthenticationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationServiceApplication.class, args);
 	}
-
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
 }
